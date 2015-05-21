a1bac5a785a1dc0ef1bc4bd58d10d99377a280f0
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2015-05-21 16:30:11 +0200

    font: add support for other code pages

--- font/alias.go
+++ font/alias.go
@@ -16,3 +16,35 @@ var fontAlias = map[string]string{
 	"IBM VGA":            "cp437",
 	"IBM VGA50":          "cp437",
 }
+
+var codePages = []string{
+	"437",
+	"720",
+	"737",
+	"775",
+	"819",
+	"850",
+	"852",
+	"855",
+	"857",
+	"858",
+	"860",
+	"861",
+	"862", // Hebrew
+	"863", // French Canada
+	"864", // Arabic
+	"865", // Nordic
+	"866", // Cyrillic
+	"869", // Greek 2
+	"872", // Cyrillic
+}
+
+func init() {
+	for _, codePage := range codePages {
+		fontAlias["IBM VGA "+codePage] = "cp" + codePage
+		fontAlias["IBM VGA25G "+codePage] = "cp" + codePage
+		fontAlias["IBM VGA50 "+codePage] = "cp" + codePage
+		fontAlias["IBM EGA "+codePage] = "cp" + codePage
+		fontAlias["IBM EGA43 "+codePage] = "cp" + codePage
+	}
+}
